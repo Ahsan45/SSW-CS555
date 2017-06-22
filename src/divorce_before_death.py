62df9c7a73bd156785757845f93e1c9b617b7e56
limerik <erikandrewlim@gmail.com>
2017-06-22 17:39:11 -0400

    Add files via upload

---
+++ src/divorce_before_death.py
@@ -0,0 +1,24 @@
+#Erik Lim
+#SSW 555 Agile Methods for Software Development
+
+'''Module for checking if marriage came before death'''
+
+from utils import date_first
+
+def divorce_before_death_husb(fam, indiv):
+    husb = fam['HUSB']
+    if not 'DEAT' in indiv[husb]:
+        return True
+    if not 'DIV' in indiv[husb]:
+        return True
+
+    return date_first(indiv[husb]['DIV'], fam['DEAT'])
+
+def divorce_before_death_wife(fam, indiv):
+    wife = fam['WIFE']
+    if not 'DEAT' in indiv[wife]:
+        return True
+    if not 'DIV' in indiv[husb]:
+        return True
+
+    return date_first(indiv[wife]['DIV'], fam['DEAT'])
---
+++ src/marr_before_death.py
@@ -0,0 +1,20 @@
+#Erik Lim
+#SSW 555 Agile Methods for Software Development
+
+'''Module for checking if marriage came before death'''
+
+from utils import date_first
+
+def marr_before_death_husb(fam, indiv):
+    husb = fam['HUSB']
+    if not 'DEAT' in indiv[husb]:
+        return True
+
+    return date_first(indiv[husb]['MARR'], fam['DEAT'])
+
+def marr_before_death_wife(fam, indiv):
+    wife = fam['WIFE']
+    if not 'DEAT' in indiv[wife]:
+        return True
+
+    return date_first(indiv[wife]['MARR'], fam['DEAT'])
