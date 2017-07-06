47d839cbf0f2ddd6393930a09df7e5755d30ae54
mnuzzo <mnuzzo@stevens.edu>
2017-07-06 18:41:11 -0400

    Added docstrings

--- src/male_last_names.py
+++ src/male_last_names.py
@@ -1,27 +1,25 @@
 """Checks Male Last Names"""
 
 def male_last_names(inds, males):
+    """Checks male last names, returns appropriate Boolean for if all male last names consistent"""
     lastNames = []
     for male in males:
-        lastNames.append(get_last_name(inds, male))
-    #print lastNames    
+        lastNames.append(get_last_name(inds, male)) 
     return len(set(lastNames))==1
 
 def get_last_name(people, individual):
+    """Finds last name from Individual dictionary"""
     surname = people[individual]['NAME'].split()
-    #print surname
     return surname[1]
 
 def get_males(families, people):
-    #print families
+    """Finds all males in family: Husband and any children who have 'SEX' of 'M'"""
         
     familyMen = []
     familyMen.append(families['HUSB'])
     children = []
     children = families['CHIL']
-    #print children
     for kid in children:
         if people[kid]['SEX'] == 'M':
-            familyMen.append(kid)
-    #print familyMen  
+            familyMen.append(kid) 
     return familyMen
