644dd712b72ff4105a0c50269de43c4712dda2a6
limerik <erikandrewlim@gmail.com>
2017-07-06 22:18:53 -0400

    Add files via upload

---
+++ src/multiple_births.py
@@ -0,0 +1,20 @@
+#Erik Lim
+#SSW 555 Agile Methods for Software Development
+
+'''Module for checking if multple births are less than or equal to 5'''
+
+from utils import date_first
+
+def multiple_births(indiv, fam):
+    if not 'CHIL' in fam:
+        return True
+
+    children =fam['CHIL']
+
+    for child1 in children:
+        i = 0
+        for child2 in children:
+            if indiv[child1]['BIRT'] == indiv[child2]['BIRT']:
+                i+=1
+            if i == 6:
+                return False
